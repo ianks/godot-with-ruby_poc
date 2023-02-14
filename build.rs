e620ac88e3c80951d4fabb9e5d48ac04aa746723
Ian Ker-Seymer <hello@ianks.com>
2023-02-14 00:10:24 -0500

    Working demo

---
+++ build.rs
@@ -0,0 +1,6 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let _ = rb_sys_env::activate()?;
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,20 @@ struct Ruby;
 #[methods]
 impl Ruby {
     fn new(_owner: &Node) -> Self {
-        Ruby
+        Self
     }
 
     #[method]
     fn _ready(&self) {
+        godot_print!("Loading ruby");
+        godot_print!("Ruby loaded");
+        let _cleanup = unsafe { embed::setup() };
         let val: f64 = eval!("a + rand", a = 1).unwrap();
         godot_print!("Random Number from Ruby: {}", val);
-        godot_print!("Hello World");
     }
 }
 
 fn init(handle: InitHandle) {
-    let _cleanup = unsafe { embed::setup() };
     handle.add_class::<Ruby>();
 }
 
